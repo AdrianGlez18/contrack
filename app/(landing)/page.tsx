a470dca886b1ef99c571420e81e5bfc3129ab32f
AdrianGlez18 <adrianglezhdez18@gmail.com>
2025-01-15 20:02:24 +0000

    Landing page button disabled

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -106,7 +106,7 @@ export default function LandingPage() {
       </main>
 
       {/* Footer */}
-      <footer className="border-t py-6 md:py-0">
+      {/* <footer className="border-t py-6 md:py-0">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
           <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
             <Library className="h-6 w-6" />
@@ -133,7 +133,7 @@ export default function LandingPage() {
             </p>
           </div>
         </div>
-      </footer>
+      </footer> */}
     </div>
   )
 }
